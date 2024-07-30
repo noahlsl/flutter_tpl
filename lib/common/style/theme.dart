d9c10ff6cd7329d46515a82dcafdca4aad433031
noah <noah9527@gmail.com>
2024-07-30 14:04:27 +0800

    feat:update

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'index.dart';
 
 /// 主题
 class AppTheme {
   /// 亮色
   static ThemeData light = ThemeData(
-    // 系统主题
-    // colorScheme: lightColorScheme,
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-    useMaterial3: true,
     // 默认字体设置(若使用系统字体则不设置)
-    fontFamily: "Montserrat",
+    // fontFamily: "Montserrat",
+    useMaterial3: true,
     appBarTheme: const AppBarTheme(
-      systemOverlayStyle: SystemUiOverlayStyle.dark, // appBar 暗色 , 和主题色相反
+      scrolledUnderElevation: 0.0,
+    ),
+    // 系统主题
+    colorScheme: ColorScheme.light(
+      primary: const Color(0xFF3CA9FC),
+      onPrimary: Colors.white,
+      secondary: const Color(0xFFA8C1D2).withOpacity(0.25),
+      tertiary: const Color(0xFFEE4667),
+      inversePrimary: Colors.grey.shade600,
+      inverseSurface: Colors.grey.shade200,
+      onInverseSurface: Colors.grey.shade100,
+      surfaceContainer: Colors.grey.shade300,
     ),
   );
 
   /// 暗色
   static ThemeData dark = ThemeData(
-    // 系统主题
-    // colorScheme: darkColorScheme,
     // 默认字体设置(若使用系统字体则不设置)
-    fontFamily: "Montserrat",
+    // fontFamily: "Montserrat",
+    useMaterial3: true,
     appBarTheme: const AppBarTheme(
-      systemOverlayStyle: SystemUiOverlayStyle.light, // appBar 亮色 , 和主题色相反
+      scrolledUnderElevation: 0.0,
+    ),
+    // 系统主题
+    colorScheme: ColorScheme.dark(
+      primary: const Color(0xFF3CA9FC),
+      onPrimary: Colors.white,
+      secondary: const Color(0xFFA8C1D2).withOpacity(0.25),
+      tertiary: const Color(0xFFEE4667),
+      inversePrimary: Colors.grey.shade600,
+      inverseSurface: Colors.black87,
+      onInverseSurface: Colors.black26,
+      surfaceContainer: Colors.grey.shade300,
     ),
   );
 }
